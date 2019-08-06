03f53dde1891c900f3d904f8078a5ca3c3711b3a
sysdevguru <sysdevguru76@gmail.com>
2019-08-06 05:40:56 +0200

    debug5

--- portmon/main.c
+++ portmon/main.c
@@ -512,7 +512,7 @@ int poll_devices()
                 fprintf(stderr, "%lld elapsedsecs, val = %.2f \n",    harray[i]->elapsedsecs, val);
 							  json_snips_log((harray[i])->hname, 3,
                    "status",  (char *) &harray[i]->status,
-				   "level",	 &v.loglevel,
+				   "level",	 (char *) v.loglevel,
                    "conntime",  (char *) &val
                            );
 
