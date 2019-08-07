9f002a8a1c6309aa604d820a11292f4eab73ed61
sysdevguru <sysdevguru76@gmail.com>
2019-08-07 19:08:53 +0200

    debug48

--- lib/snips_json.c
+++ lib/snips_json.c
@@ -53,12 +53,10 @@ void json_snips_log(char *parent, int count, ...) {
     value = (float *)va_arg(argp, char *);
 
     if (value != 0) {
-        if (curCount < 1)
-        fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, *((int *)value), count > 1 ? ',' : ' ');
-		if else (curCount = 1)
-		fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, *((int *)value), count > 1 ? ',' : ' ');
-        else 
-        fprintf(jsonf, "\n\t\t\t\t\"%s\": %.9f%c", name, *((float *)value), count > 1 ? ',' : ' ');
+        if (curCount <= 1) {
+       	fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, *((int *)value), count > 1 ? ':' : ' ');
+		else
+       	fprintf(jsonf, "\n\t\t\t\t\"%s\": %.9f%c", name, *((float *)value), count > 1 ? ':' : ' ');
 
     } else {
       fprintf(jsonf, "\n\t\t\t\t\"%s\": null%c", name, count > 1 ? ',' : ' ');
