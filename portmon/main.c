ae65a1e982fbb6b419af92332e336d8efe86639a
sysdevguru <sysdevguru76@gmail.com>
2019-08-07 05:04:41 +0200

    debug31

--- portmon/main.c
+++ portmon/main.c
@@ -513,7 +513,7 @@ int poll_devices()
 							  
 				json_snips_logs((harray[i])->hname, 3,
                    "status",  (char *) &harray[i]->status,
-				   "level",	 (char *) &v.loglevel,
+				   "level1",	 v.loglevel,
                    "conntime",  (char *) &val
                            );
 
