2065be9640b54720264869ba1dd1bdeb17a0432a
sysdevguru <sysdevguru76@gmail.com>
2019-08-07 04:28:55 +0200

    debug24

--- lib/snips_json.c
+++ lib/snips_json.c
@@ -54,12 +54,12 @@ void json_snips_log(char *parent, int count, ...) {
 
     if (value != 0) {
         if (curCount < 1)
-        fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, *((int *)value), count > 1 ? ',,' : ' ');
+        fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, *((int *)value), count > 1 ? ',' : ' ');
         else
-        fprintf(jsonf, "\n\t\t\t\t\"%s\": %.9f%c", name, *((float *)value), count > 1 ? ',,' : ' ');
+        fprintf(jsonf, "\n\t\t\t\t\"%s\": %.9f%c", name, *((float *)value), count > 1 ? ',' : ' ');
 
     } else {
-      fprintf(jsonf, "\n\t\t\t\t\"%s\": null%c", name, count > 1 ? ',,' : ' ');
+      fprintf(jsonf, "\n\t\t\t\t\"%s\": null%c", name, count > 1 ? ',' : ' ');
     }
 
                 curCount++;
@@ -90,12 +90,12 @@ void json_log(char *parent, int count, ...) {
 
     if (value != 0) {
     	if (curCount <= 1)
-            fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, (int) *value, count > 1 ? ',' : ' ');
+            fprintf(jsonf, "\n\t\t\t\t\"%s\": %d%c", name, (int) *value, count > 1 ? ',,' : ' ');
         else
-            fprintf(jsonf, "\n\t\t\t\t\"%s\": %.2f%c", name, *value, count > 1 ? ',' : ' ');
+            fprintf(jsonf, "\n\t\t\t\t\"%s\": %.2f%c", name, *value, count > 1 ? ',,' : ' ');
 
     } else {
-      fprintf(jsonf, "\n\t\t\t\t\"%s\": null%c", name, count > 1 ? ',' : ' ');
+      fprintf(jsonf, "\n\t\t\t\t\"%s\": null%c", name, count > 1 ? ',,' : ' ');
     }
 
 		curCount++;
