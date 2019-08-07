48d19d312ff61a37f4b86f98385364426c213b8a
sysdevguru <sysdevguru76@gmail.com>
2019-08-07 21:47:43 +0200

    debug62

--- snipslog/snipslogd.c
+++ snipslog/snipslogd.c
@@ -361,8 +361,7 @@ int readevent(fd)
 	  }
 	}
         memset(fullLogStr, 0x00, sizeof(fullLogStr));
-		v.var.threshold = v.var.threshold / 1000.0;
-		fprintf(stderr, "threshold '%s'\n", v);
+		v.var.threshold = v.var.threshold/1000.0;
         char *logStr = event_to_logstr(&v);
         if (logStr) {
             logStr[strlen(logStr) - 1] = '\0';
